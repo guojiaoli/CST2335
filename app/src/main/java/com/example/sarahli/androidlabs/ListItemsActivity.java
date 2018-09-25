d9e6c9b176ddc1054bdbc5b82d01feb12b198fb7
guojiaoli <Qazlgj311>
2018-09-24 20:48:27 -0400

    Finished lab 3

--- app/src/main/java/com/example/sarahli/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/sarahli/androidlabs/ListItemsActivity.java
@@ -1,13 +1,135 @@
 package com.example.sarahli.androidlabs;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class ListItemsActivity extends Activity {
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static ImageButton b3;
+    static Switch s1;
+    static CheckBox c1;
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public void setOnCheckedChanged(){
+        if (s1.isChecked()) {
+            CharSequence text = "Switch is On";// "Switch is Off"
+            int duration = Toast.LENGTH_SHORT; //= Toast.LENGTH_LONG if Off
+
+            Toast toast = Toast.makeText(this, text, duration); //this is the ListActivity
+            toast.show(); //display your message box
+        } else{
+            CharSequence text = "Switch is Off";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(this, text, duration); //this is the ListActivity
+            toast.show(); //display your message box
+        }
+    }
+
+    public void OnCheckChanged(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+// 2. Chain together various setter methods to set the dialog characteristics
+
+        builder.setMessage(R.string.dialog_message) //Add a dialog message to strings.xml
+
+                .setTitle(R.string.dialog_title)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                        Intent resultIntent = new Intent(  );
+                        resultIntent.putExtra("Response",getString(R.string.response));
+                        setResult(Activity.RESULT_OK, resultIntent);
+                        finish();
+
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                })
+                .show();
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(ACTIVITY_NAME,"In onCreate()");
+
         setContentView(R.layout.activity_list_items);
+        b3 = (ImageButton)findViewById(R.id.imageButton);
+
+        s1 = (Switch)findViewById(R.id.switch1) ;
+
+        c1 = (CheckBox)findViewById(R.id.checkBox1) ;
+
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v3) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        s1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v4) {
+                setOnCheckedChanged();
+            }
+        });
+
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v5) {
+                OnCheckChanged();
+            }
+        });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            b3.setImageBitmap(imageBitmap);
+        }
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME,"In onResume()");}
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME,"In onStart()");}
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME,"In onPause()");}
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME,"In onStop()");}
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME,"In onDestroy()");}
 }
